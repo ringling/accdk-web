1d2eae1d56062f76fa60da04750908466abe784c
Thomas Ringling <thomas@ringling.info>
2019-10-04 09:05:28 +0200

    Page title

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Agile Coach Camp',
   plugins: [],
   siteUrl: 'https://ringling.github.io',
   pathPrefix: '/'
